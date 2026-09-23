1ef1e98030a66205ad0ca67156e7eb073fcbc98e
Ali Syed <thealisyed@gmail.com>
2026-09-23 12:56:30 +0100

    OCPBUGS-27741: Escape rewrite path regex

--- pkg/router/router_test.go
+++ pkg/router/router_test.go
@@ -1039,6 +1039,44 @@ func TestConfigTemplate(t *testing.T) {
 				},
 			},
 		},
+		"Rewrite target with regex metacharacter in path": {
+			mustCreateWithConfig{
+				mustCreateRoute: mustCreateRoute{
+					name: "rewrite-literal-plus",
+					host: "rewrite-literal-plus.example.com",
+					path: "/bar+",
+					time: start,
+					annotations: map[string]string{
+						"haproxy.router.openshift.io/rewrite-target": "/foo",
+					},
+				},
+				mustMatchConfig: mustMatchConfig{
+					section:     "backend",
+					sectionName: insecureBackendName(h.namespace, "rewrite-literal-plus"),
+					attribute:   "http-request",
+					value:       `replace-path '^/bar\+(.*)$' '/foo\1'`,
+				},
+			},
+		},
+		"Rewrite target with dot in path": {
+			mustCreateWithConfig{
+				mustCreateRoute: mustCreateRoute{
+					name: "rewrite-literal-dot",
+					host: "rewrite-literal-dot.example.com",
+					path: "/api/v1.0",
+					time: start,
+					annotations: map[string]string{
+						"haproxy.router.openshift.io/rewrite-target": "/v2",
+					},
+				},
+				mustMatchConfig: mustMatchConfig{
+					section:     "backend",
+					sectionName: insecureBackendName(h.namespace, "rewrite-literal-dot"),
+					attribute:   "http-request",
+					value:       `replace-path '^/api/v1\.0(.*)$' '/v2\1'`,
+				},
+			},
+		},
 	}
 
 	defer cleanUpRoutes(t)
